2ef77bac4e0f9af871a6ae878c79a9da316d4758
KhoiDOO <khoido8899@gmail.com>
2024-03-13 19:38:22 +0700

    init

--- main.py
+++ main.py
@@ -14,15 +14,16 @@ if __name__ == "__main__":
     parser.add_argument('--wk', type=int, required=True, help='no. ds worker')
     parser.add_argument('--pm', action='store_true', help='toggle to use pin memory')
 
-    # domain
+    # domain - tasks - losses
     parser.add_argument('--trdms', type=int, nargs='+', required=True, default=[0], help='list of domain used in training')
-    
-    # task
     parser.add_argument('--trtks', type=str, nargs='+', required=True, help='list of tasks used in training')
+    parser.add_argument('--losses', type=str, nargs='+', required=True, help='losses of tasks used in training')
 
     # method
+    parser.add_argument('--m', type=str, required=True, help='method used in training')
 
     # model
+    parser.add_argument('--model', type=str, required=True, help='model type (i.e. ae, hps (hard parameter sharing))')
     parser.add_argument('--at', type=str, required=True, help='archiecture type (i.e. ae, unet)')
     parser.add_argument('--bb', type=str, required=True, help='backbone type (i.e. ae, base, resnet18)')
 
@@ -30,5 +31,14 @@ if __name__ == "__main__":
     parser.add_argument('--seed', type=int, default=0, help='Seed for everything else')
     parser.add_argument('--tm', type=str, default='sup', help='training mode (i.e. supervised)')
     parser.add_argument('--dvids', type=str, nargs='+', default=[0], help='list of device used in training')
+    parser.add_argument('--epoch', type=int, default=1, help='number of epochs used in training')
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    parser.add_argument('--port', type=int, default=8080, help='Multi-GPU Training Port.')
+
+    # logging
+    parser.add_argument('--wandb', action='store_true', help='toggle to use wandb for online saving')
+    parser.add_argument('--log', action='store_true', help='toggle to use tensorboard for offline saving')
+    parser.add_argument('--wandb_prj', type=str, required=True, help='wandb project name')
+    parser.add_argument('--wandb_entity', type=str, required=True, help='wandb entity name')
 
     args = parser.parse_args()
