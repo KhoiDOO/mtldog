24728b13754306a8dc487e11fabd507c06f83425
KhoiDOO <khoido8899@gmail.com>
2024-03-15 21:54:22 +0700

    fix BUG wrong implementation

--- loss/cls/focal.py
+++ loss/cls/focal.py
@@ -5,11 +5,11 @@ from argparse import Namespace
 
 def loss_cls_focal(logits: Tensor, labels:Tensor, args: Namespace):
     
-    prob = F.log_softmax(logits, dim=1)
+    log_prob = F.log_softmax(logits, dim=1)
 
     _, C = tuple(logits.size())
 
-    entropy = torch.pow(1 - prob.exp(), args.gamma) * logits * F.one_hot(labels, num_classes=C).float()
+    entropy = torch.pow(1 - log_prob.exp(), args.gamma) * log_prob * F.one_hot(labels, num_classes=C).float()
 
     loss = (-1) * entropy.mean()
 
