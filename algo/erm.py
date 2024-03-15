82d82fb39d25386a8e925bc0f1ed95cf7f9ec23e
KhoiDOO <khoido8899@gmail.com>
2024-03-15 14:30:56 +0700

    update ERM method

---
+++ algo/erm.py
@@ -0,0 +1,16 @@
+from argparse import Namespace
+from typing import List
+from .core import MTLDOGALGO
+
+import torch
+
+
+class ERM(MTLDOGALGO):
+    def __init__(self):
+        super().__init__()
+
+    def backward(self, losses: List, args: Namespace):
+        total_loss = torch.sum(losses)
+        total_loss.backward()
+
+        return None
