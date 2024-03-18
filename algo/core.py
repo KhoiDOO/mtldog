ee4c42553e3cf5d3acde2e5c0b91bf0cf8301225
KhoiDOO <khoido8899@gmail.com>
2024-03-19 00:20:16 +0700

    update head params dimmension calculation functions

--- algo/core.py
+++ algo/core.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 from argparse import Namespace
 from torch import nn, Tensor
 
@@ -15,13 +15,26 @@ class MTLDOGALGO(nn.Module):
         self.hparams_path = args.hp
         self.params = json.load(open(self.hparams_path, 'r'))
 
-    def _compute_grad_dim(self):
-        self.grad_index = []
+    def compute_grad_dim_share(self):
+        self.grad_index_share : List[int] = []
         for param in self.get_share_params():
-            self.grad_index.append(param.data.numel())
-        self.grad_dim = sum(self.grad_index)
+            self.grad_index_share.append(param.data.numel())
+        self.grad_dim_share : int = sum(self.grad_index)
+    
+    def compute_grad_dim_heads(self):
+        self.grad_index_heads : Dict[str, List[int]] = {}
+        head_params_dict : Dict[str, List[Tensor]] = self.get_heads_params()
+        
+        for head in head_params_dict:
+            for param in head_params_dict[head]:
+                if head in self.grad_index_heads:
+                    self.grad_index_heads[head] = [param.data.numel()]
+                else:
+                    self.grad_index_heads[head].append(param.data.numel())
+        
+        self.grad_dim_heads : Dict[str, List[int]] = {head : sum(self.grad_index_heads[head]) for head in self.grad_index_heads}
 
-    def _grad2vec(self):
+    def grad2vec_share(self):
         grad = torch.zeros(self.grad_dim)
         count = 0
         for param in self.get_share_params():
@@ -32,12 +45,12 @@ class MTLDOGALGO(nn.Module):
             count += 1
         return grad
     
-    def _compute_grad(self, losses: Tensor, mode: str):
+    def compute_grad_share(self, losses: Tensor, mode: str):
         grads = torch.zeros(self.task_num, self.grad_dim).to(self.device)
         for tn in range(self.task_num):
             if mode == 'backward':
                 losses[tn].backward(retain_graph=True) if (tn+1)!=self.task_num else losses[tn].backward()
-                grads[tn] = self._grad2vec()
+                grads[tn] = self.grad2vec_share()
             elif mode == 'autograd':
                 grad = list(torch.autograd.grad(losses[tn], self.get_share_params(), retain_graph=True))
                 grads[tn] = torch.cat([g.view(-1) for g in grad])
@@ -46,7 +59,7 @@ class MTLDOGALGO(nn.Module):
             self.zero_grad_share_params()
         return grads
     
-    def _reset_grad(self, new_grads):
+    def reset_grad(self, new_grads):
         count = 0
         for param in self.get_share_params():
             if param.grad is not None:
@@ -55,17 +68,17 @@ class MTLDOGALGO(nn.Module):
                 param.grad.data = new_grads[beg:end].contiguous().view(param.data.size()).data.clone()
             count += 1
         
-    def _get_grads(self, losses: Tensor, mode: str='backward'):
+    def get_grads(self, losses: Tensor, mode: str='backward'):
         self._compute_grad_dim()
         grads = self._compute_grad(losses, mode)
         return grads
     
-    def _backward_new_grads(self, batch_weight, grads=None):
+    def backward_new_grads(self, batch_weight, grads=None):
         new_grads = sum([batch_weight[i] * grads[i] for i in range(self.task_num)])
         self._reset_grad(new_grads)
     
 
-    def backward(self, losses: List, args: Namespace):
+    def backward(self, losses: Dict[str, List[Tensor]]):
         raise NotImplementedError()
 
     @property
