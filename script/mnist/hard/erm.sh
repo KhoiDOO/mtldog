d894b43538420ac90a3c3f55d01de8d491498d32
KhoiDOO <khoido8899@gmail.com>
2024-03-23 16:15:41 +0700

    update training script

---
+++ script/mnist/hard/erm.sh
@@ -0,0 +1,10 @@
+python main.py --ds mnist --dt ./ds/src --bs 64 --wk 12 --pm \
+    --trdms 0 1 \
+    --tkss rec cls \
+    --losses mse ce \
+    --m erm --hp ./hparams/erm.json \
+    --model hps --at ae --bb base \
+    --seed 0 --tm sup \
+    --dvids 1 \
+    --round 2 --chkfreq 1 --lr 0.001 \
+    # --wandb --log --wandb_prj MTLDOG --wandb_entity heartbeats
