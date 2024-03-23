f0bf7152b3f2622082a21c336bfa5c604bebfe1a
KhoiDOO <khoido8899@gmail.com>
2024-03-23 16:11:31 +0700

    fix BUG wrong size reconstruction head

--- arch/subnet/mnist.py
+++ arch/subnet/mnist.py
@@ -63,7 +63,7 @@ class MNIST_HPS_REC_AE_BASE_DECODER(nn.Module):
             nn.ConvTranspose2d(128, 128, 3, stride=1, padding=1),
             nn.ReLU(),
             nn.GroupNorm(8, 128),
-            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1),
+            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
             nn.GroupNorm(8, 64),
             nn.ConvTranspose2d(64, 1, 3, stride=1, padding=1),
