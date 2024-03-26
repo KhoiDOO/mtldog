f3b10f25b4eb1adbdaed9ddcaa85ffae58434776
KhoiDOO <khoido8899@gmail.com>
2024-03-26 16:51:26 +0700

    update cityscapes check

--- ds/city.py
+++ ds/city.py
@@ -28,21 +28,48 @@ class CityScapes(MTLDOGDS):
 
         super().__init__(root_dir, domain, tasks, train, default_domains, default_tasks, dm2idx)
 
-        if domain == 'normal':
+        if domain == 0:
             if self.tr:
                 img_dir = root_dir + '/leftImg8bit_trainvaltest/leftImg8bit/train'
             else:
                 img_dir = root_dir + '/leftImg8bit_trainvaltest/leftImg8bit/val'
-        elif domain == 'foggy':
+        elif domain == 1:
             if self.tr:
                 img_dir = root_dir + '/leftImg8bit_trainvaltest_foggy/leftImg8bit_foggy/train'
             else:
                 img_dir = root_dir + '/leftImg8bit_trainvaltest_foggy/leftImg8bit_foggy/val'
-        elif domain == 'rainy':
+        elif domain == 2:
             if self.tr:
                 img_dir = root_dir + '/leftImg8bit_trainval_rain/leftImg8bit_rain/train'
             else:
                 img_dir = root_dir + '/leftImg8bit_trainval_rain/leftImg8bit_rain/val'
         
-        img_paths = glob(img_dir + "/*/*")
+        self.img_paths = glob(img_dir + "/*/*")
+    
+    def __len__(self):
+        return len(self.img_paths)
 
+def ds_city(args: Namespace) -> tuple[List[CityScapes], List[CityScapes]]:
+    pass
+
+# CHECK
+
+"""
+ROOT = "/media/mountHDD3/data_storage/cityscapes/unzip"
+
+tr_norm_ds = CityScapes(root_dir=ROOT, domain=0, train=True)
+tr_fogg_ds = CityScapes(root_dir=ROOT, domain=1, train=True)
+tr_rain_ds = CityScapes(root_dir=ROOT, domain=2, train=True)
+
+te_norm_ds = CityScapes(root_dir=ROOT, domain=0, train=False)
+te_fogg_ds = CityScapes(root_dir=ROOT, domain=1, train=False)
+te_rain_ds = CityScapes(root_dir=ROOT, domain=2, train=False)
+
+print(len(tr_norm_ds))
+print(len(tr_fogg_ds))
+print(len(tr_rain_ds))
+
+print(len(te_norm_ds))
+print(len(te_fogg_ds))
+print(len(te_rain_ds))
+"""
