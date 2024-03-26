fb7d54774c1b054a430b301d7ca48308cba322e0
KhoiDOO <khoido8899@gmail.com>
2024-03-27 00:04:17 +0700

    fix BUG unstable (-inf) psnr metric

--- metric/rec/psnr.py
+++ metric/rec/psnr.py
@@ -5,4 +5,10 @@ import torch.nn.functional as F
 def metric_rec_psnr(pred: Tensor, target: Tensor) -> float:
     mse = F.mse_loss(pred, target)
 
-    return (20 * torch.log10(torch.max(pred) / torch.sqrt(mse))).item()
+    return (
+        20 * torch.log10(
+            torch.clamp(
+                torch.max(pred) / torch.sqrt(mse), min=0.001
+            )
+        )
+    ).item()
