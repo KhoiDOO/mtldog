ea37beffc1d42cbd4a5a755c4ffc0a6b04420833
KhoiDOO <khoido8899@gmail.com>
2024-03-28 15:47:04 +0700

    update training script for cityscapes training

---
+++ script/city/normal/erm/erm.sh
@@ -0,0 +1,7 @@
+python main.py --ds city_normal --dt ./ds/src --bs 32 --wk 8 \
+    --trdms 0 --tkss seg depth --losses ce mse \
+    --m erm --hp ./hparams/erm.json \
+    --model hps --at segnet --bb basenano \
+    --lr 0.001 --seed 0 \
+    --tm sup --dvids 0 --port 7777 \
+    --round 5000 --chkfreq 100
---
+++ script/city/normal/erm/erm_wandb.sh
@@ -0,0 +1,8 @@
+python main.py --ds city_normal --dt ./ds/src --bs 32 --wk 8 \
+    --trdms 0 --tkss seg depth --losses ce mse \
+    --m erm --hp ./hparams/erm.json \
+    --model hps --at segnet --bb basenano \
+    --lr 0.001 --seed 0 \
+    --tm sup --dvids 0 --port 7777 \
+    --round 1000 --chkfreq 100 \
+    --wandb --wandb_prj MTLDOG --wandb_entity heartbeats
