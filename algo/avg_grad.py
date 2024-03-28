4492d0fa8a18566109609f15411a65b8c83d1a78
namkhanh <namkhanh2172@gmail.com>
2024-03-28 14:27:08 +0700

    Refactor backward method in AVG_GRAD class

--- algo/avg_grad.py
+++ algo/avg_grad.py
@@ -13,16 +13,16 @@ class AVG_GRAD(MTLDOGALGO):
     def backward(self, losses: Dict[str, Tensor]):
         
         share_grads = torch.stack([self.get_grads_share(loss, mode='backward') for loss in losses.values()])
-        # mean_share_grads = torch.mean(share_grads)
-        mean_share_grads = share_grads[0]
-        self.reset_grad_share(mean_share_grads)
+        mean_share_grads = torch.mean(share_grads, dim=[0,1])
+        # mean_share_grads = share_grads[0][0]
         
         head_grad = []
         for loss in losses.values():
             head_grad.append(self.get_grads_heads(loss, mode='backward'))
-        mean_head_grad = {head: torch.mean(torch.stack([grad[head] for grad in head_grad]), dim=0) for head in self.args.tkss}
-        self.reset_grad_heads(head_grad[0])
-        print(self.get_grads_share)
+        mean_head_grad = {head: torch.mean(torch.stack([grad[head] for grad in head_grad]), dim=0).to(device=self.device) for head in self.args.tkss}
+        
+        self.reset_grad_share(mean_share_grads)
+        self.reset_grad_heads(mean_head_grad)
         
         return None, None
 
