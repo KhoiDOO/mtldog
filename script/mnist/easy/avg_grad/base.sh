9567cc8aee6a069376f0ef14d1234d09ce4dfccb
namkhanh <namkhanh2172@gmail.com>
2024-03-28 14:27:08 +0700

    Refactor backward method in AVG_GRAD class

---
+++ script/mnist/easy/avg_grad/base.sh
@@ -0,0 +1,7 @@
+python main.py --ds mnisteasy --dt ./ds/src --bs 64 --wk 8 \
+    --trdms 0 1 --tkss rec cls --losses mse ce \
+    --m avg_grad --hp ./hparams/erm.json \
+    --model hps --at ae --bb base \
+    --lr 0.001 --seed 0 \
+    --tm sup --dvids 0 --port 7777 \
+    --round 5000 --chkfreq 100
