0c8534ccc6fdb2af285b5af7a432b7fc6d55592f
KhoiDOO <khoido8899@gmail.com>
2024-03-30 15:31:15 +0700

    remove redundant file

--- script/city/full/erm/erm_grad.sh
+++
@@ -1,7 +0,0 @@
-python main.py --ds city --dt /media/mountHDD3/data_storage/cityscapes/unzip --bs 32 --wk 8 \
-    --trdms 0 1 --tkss seg depth --losses ce mse \
-    --m erm --hp ./hparams/erm.json \
-    --model hps --at segnet --bb basenano \
-    --lr 0.001 --seed 0 \
-    --tm sup --dvids 0 --port 7777 \
-    --round 2 --chkfreq 1 --grad \
--- script/city/normal/erm/erm.sh
+++
@@ -1,7 +0,0 @@
-python main.py --ds citynormal --dt /media/mountHDD3/data_storage/cityscapes/unzip --bs 32 --wk 8 \
-    --trdms 0 --tkss seg depth --losses ce mse \
-    --m erm --hp ./hparams/erm.json \
-    --model hps --at segnet --bb basenano \
-    --lr 0.001 --seed 0 \
-    --tm sup --dvids 0 --port 7777 \
-    --round 5000 --chkfreq 100
