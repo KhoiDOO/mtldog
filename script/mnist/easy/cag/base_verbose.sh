7be2c7ec85faca56ba1522a9bc1acef9a439ac04
KhoiDOO <khoido8899@gmail.com>
2024-04-04 22:50:28 +0700

    remove old script dir

--- script/mnist/easy/cag/base_verbose.sh
+++
@@ -1,7 +0,0 @@
-python main.py --ds mnisteasy --dt ./ds/src --bs 64 --wk 8 \
-    --trdms 0 1 2 --tkss rec cls --losses mse ce \
-    --m erm --hp ./hparams/cag.json \
-    --model hps --at ae --bb base \
-    --lr 0.001 --seed 0 \
-    --tm sup --dvids 0 --port 8080 \
-    --round 5000 --chkfreq 5000 --verbose
