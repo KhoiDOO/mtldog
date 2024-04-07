cdf50f906caca8a76f82aa7fbc221c4aee4b6b1e
KhoiDOO <khoido8899@gmail.com>
2024-04-07 11:26:34 +0700

    strict psnr

--- mtldog/metric/rec/psnr.py
+++ mtldog/metric/rec/psnr.py
@@ -2,13 +2,13 @@ from torch import Tensor
 import torch
 import torch.nn.functional as F
 
-def metric_rec_psnr(pred: Tensor, target: Tensor) -> float:
-    mse = F.mse_loss(pred, target)
+def metric_rec_psnr(preds: Tensor, label: Tensor) -> float:
+    mse = F.mse_loss(preds, label)
 
     return (
         20 * torch.log10(
             torch.clamp(
-                torch.max(pred) / torch.sqrt(mse), min=0.001
+                torch.max(preds) / torch.sqrt(mse), min=0.001
             )
         )
     ).item()
