d7bca3087066d85aa6c72b937ee81a0b25592122
KhoiDOO <khoido8899@gmail.com>
2024-04-11 00:58:42 +0700

    compact and add wandb cache dir

--- mtldog/main.py
+++ mtldog/main.py
@@ -1,12 +1,38 @@
 from argparse import Namespace
 
+import os
 import argparse
 import random, torch
 import numpy as np
 import json
 
+def prepare():
+    cache_dir = os.environ['WANDB_CACHE_DIR'] = os.getcwd() + "/.cache"
+    if not os.path.exists(cache_dir):
+        os.mkdir(cache_dir)
+
+    wandb_cache_dir = cache_dir + "/wandb"
+    if not os.path.exists(wandb_cache_dir):
+        os.mkdir(wandb_cache_dir)
+    
+    os.environ['WANDB_CACHE_DIR'] = wandb_cache_dir
+
+def train_init(args):
+    if args.tm == 'sup':
+        from train import SUP
+        trainer = SUP(args=args)
+    elif args.tm == 'sinsup':
+        from train import SINSUP
+        trainer = SINSUP(args=args)
+    else:
+        raise ValueError(f"training mode {args.tm} is not available")
+
+    trainer.run()
 
 if __name__ == "__main__":
+
+    prepare()
+
     parser = argparse.ArgumentParser(prog='MTLDOG - Domain Generalization for Multi-task Learning')
 
     parser.add_argument('--cfp', type=str, default="./hparams/example.json", help='Configuration path for training')
@@ -22,13 +48,4 @@ if __name__ == "__main__":
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-    if args.tm == 'sup':
-        from train import SUP
-        trainer = SUP(args=args)
-    elif args.tm == 'sinsup':
-        from train import SINSUP
-        trainer = SINSUP(args=args)
-    else:
-        raise ValueError(f"training mode {args.tm} is not available")
-
-    trainer.run()
+    train_init(args=args)
